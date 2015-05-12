f8504312dc6c3ac46271762d216b3029ce0427ed
Lukasz Tracz <kapicel20wp.pl>
2015-05-12 21:30:37 +0200

    Test 1 red

---
+++ Desktop/Kata/loadBalancerKataMto.v1/src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.serverloadbalancer;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class ServerLoadBalancerTest {
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));
+	}
+
+	
+	@Test
+	public void balancingAServer_noVms_serverStaysEmpty(){
+		Server theServer = a(server().withCapacity(1));
+		
+		balance(aListOfServersWith(theServer), anEmptyListOfVms());
+		
+		assertThat(theServer, hasLoadPercentageOf(0.0d));
+	}
+}
+
+}
