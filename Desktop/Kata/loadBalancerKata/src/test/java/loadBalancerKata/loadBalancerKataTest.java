2e44eaee04dfd9b9698dbb026c079db3799fca09
Lukasz Tracz <kapicel20wp.pl>
2015-05-12 20:16:32 +0200

    Test 1 red.

--- Desktop/Kata/loadBalancerKata/src/test/java/loadBalancerKata/loadBalancerKataTest.java
+++ Desktop/Kata/loadBalancerKata/src/test/java/loadBalancerKata/loadBalancerKataTest.java
@@ -12,6 +12,13 @@ public class loadBalancerKataTest {
 		assertThat(true, equalTo(true));
 	}
 
-
+	@Test
+	public void balancingAServer_noVms_serverStaysEmpty(){
+		Server theServer = a(server().withCapacity(1));
+		
+		balance(aListOfServersWith(theServer), anEmptyListOfVms());
+		
+		assertThat(theServer, hasLoadPercentageOf(0.0d));
+	}
 
 }
