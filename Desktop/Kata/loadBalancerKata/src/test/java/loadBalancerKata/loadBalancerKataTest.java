9d10e821227a24d3c8699ffeb0db7217bb8bd5d3
Lukasz Tracz <kapicel20wp.pl>
2015-05-12 20:13:01 +0200

    zero test itCompiles

---
+++ Desktop/Kata/loadBalancerKata/src/test/java/loadBalancerKata/loadBalancerKataTest.java
@@ -0,0 +1,17 @@
+package loadBalancerKata;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class loadBalancerKataTest {
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));
+	}
+
+
+
+}
